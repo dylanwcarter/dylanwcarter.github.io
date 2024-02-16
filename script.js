6337782f13969ee5a0ebb3d7abfb74141fe148d9
dylancrter <dylanc.rter@gmail.com>
2024-02-15 20:50:04 -0500

    Delete unused images and update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// Add interactivity here if needed
-
-// Get all the links on the page
-var links = document.querySelectorAll('a');
-
-// Add an event listener to each link
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function(event) {
-        // Prevent the default action (navigating to the link)
-        event.preventDefault();
-
-        // Do something when the link is clicked
-        alert('You clicked a link!');
-    });
-}
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
